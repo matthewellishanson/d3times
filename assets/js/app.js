d521516d3ab8be0e68e959b180eb8a71547e22cc
matthewellishanson <lifeasmeh@gmail.com>
2019-06-27 21:33:58 -0500

    instructions and modified app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,3 +16,6 @@ var svg = d3.select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
